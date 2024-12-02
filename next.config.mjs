9c79eae9ca26863560da3d7e6d2ac8c7c475e868
vunqhe173069@fpt.edu.vn <vunqhe173069@fpt.edu.vn>
2024-12-02 10:48:40 +0700

    CC

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,27 @@
 /** @type {import('next').NextConfig} */
-
-// Kiểm tra môi trường hoặc gán giá trị mặc định
-const output = process?.env?.output || "export"; // Đặt mặc định là 'export'
-const basePath = process?.env?.basePath || "/CLOUDPHONE"; // Gán đường dẫn phù hợp
-const headers =
-  output === "export"
-    ? undefined // Không cần headers nếu output là 'export'
-    : async () => {
-        return [
+const output = process?.env?.output || "standalone";
+const basePath = process?.env?.basePath || "";
+const headers = "export" === output ? undefined :async () => {
+    return [
+      {
+        headers: [
           {
-            headers: [
-              {
-                key: "Cache-Control",
-                value: "public, max-age=31536000, immutable",
-              },
-            ],
-            source:
-              "/:path(.+\\.(?:ico|png|svg|jpg|jpeg|gif|webp|json|js|css|mp3|mp4|ttf|ttc|otf|woff|woff2)$)",
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
           },
-        ];
-      };
-
+        ],
+        source:
+          "/:path(.+\\.(?:ico|png|svg|jpg|jpeg|gif|webp|json|js|css|mp3|mp4|ttf|ttc|otf|woff|woff2)$)",
+      },
+    ];
+  } 
 const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
   swcMinify: true,
-  output, // Xuất output dạng 'export'
-  basePath, // Đường dẫn gốc của ứng dụng
-  headers, // Headers chỉ áp dụng nếu không phải 'export'
+  output,
+  basePath,
+  headers,
 };
 
 export default nextConfig;
